82d45e0253ff5bd8ec1df48532241e24af6f2aef
jmalone487 <jmalone487@gmail.com>
2024-08-29 15:15:33 -0500

    Commit message

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+import satData from './satData';  // Ensure the path to satData is correct
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {/* Create buttons for each unique orbit type */}
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+
+      {/* Button to display all satellites */}
+      <button onClick={() => setSat(satData)}>
+        All Satellites
+      </button>
     </div>
   );
 };
